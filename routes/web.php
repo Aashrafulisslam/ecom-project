ea5eff3f32484a915937a5409cb29e406bdfa842
Ashraful Islam Pritom <aashrafulisslam@gmail.com>
2023-08-22 18:11:49 +0600

    22 august

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,9 @@ Route::prefix('/admin')->group(function () {
     Route::match(['get','post'],'login', [AdminController::class, 'login']);
     Route::group(['middleware' => 'admin'], function () {
         Route::get('dashboard', [AdminController::class, 'dashboard']);
+        
         Route::get('logout', [AdminController::class, 'logout']);
+
         Route::match(['get','post'], 'update-admin-password', [AdminController::class, 'updateAdminPassword']);
     });
 });
